79c0a877e158fbdf6d41c69ec0a73da087e410e2
tissaindra-tech <tissa.indra@gmail.com>
2026-04-23 23:36:49 +0800

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Ganti dengan URL dan anon key dari Supabase project Anda
-const SUPABASE_URL  = 'https://wplwokaolgfcjxtuqmyl.supabase.co'
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwbHdva2FvbGdmY2p4dHVxbXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3Mzk0NTcsImV4cCI6MjA5MjMxNTQ1N30.OBYMl5vmN6FwT7-0Qc2XHtm8pbodBSXkWk55_gt-v6Q'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
